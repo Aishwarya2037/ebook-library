fc1d1a1a7ab358865bfe4b968bedd1d33cba4443
Aishwarya Jagtap <aishwarya2037@gmail.com>
2026-04-10 12:15:20 +0530

    Update BookList.tsx

--- elibApp/elib-app/src/app/(home)/components/BookList.tsx
+++ elibApp/elib-app/src/app/(home)/components/BookList.tsx
@@ -26,39 +26,39 @@
 
 
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
-import { Book } from "@/src/types";
-import BookCard from "./BookCard";
+// import { Book } from "@/src/types";
+// import BookCard from "./BookCard";
 
-export const BookList = async () => {
-  let books: Book[] = [];
+// export const BookList = async () => {
+//   let books: Book[] = [];
 
-  try {
-    const response = await fetch(
-      "https://ebook-library-1.onrender.com/api/books?all=true",
-      {
-        cache: "no-store",
-      }
-    );
+//   try {
+//     const response = await fetch(
+//       "https://ebook-library-1.onrender.com/api/books?all=true",
+//       {
+//         cache: "no-store",
+//       }
+//     );
 
-    if (response.ok) {
-      const data = await response.json();
-      books = data.books || [];
-    }
-  } catch (error) {
-    console.error("Error fetching books:", error);
-  }
+//     if (response.ok) {
+//       const data = await response.json();
+//       books = data.books || [];
+//     }
+//   } catch (error) {
+//     console.error("Error fetching books:", error);
+//   }
 
-  return (
-    <div className="max-w-6xl mx-auto mt-6 px-4 grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mb-8">
-      {books.length > 0 ? (
-        books.map((book: Book) => (
-          <BookCard key={book._id} book={book} />
-        ))
-      ) : (
-        <p>No books found</p>
-      )}
-    </div>
-  );
-};
+//   return (
+//     <div className="max-w-6xl mx-auto mt-6 px-4 grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mb-8">
+//       {books.length > 0 ? (
+//         books.map((book: Book) => (
+//           <BookCard key={book._id} book={book} />
+//         ))
+//       ) : (
+//         <p>No books found</p>
+//       )}
+//     </div>
+//   );
+// };
