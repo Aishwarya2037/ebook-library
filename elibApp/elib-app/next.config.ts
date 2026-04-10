8f0526b3a77e9231ee3bd0d297b11b344b5208db
Aishwarya Jagtap <aishwarya2037@gmail.com>
2026-04-10 11:43:27 +0530

    Update next.config.ts

--- elibApp/elib-app/next.config.ts
+++ elibApp/elib-app/next.config.ts
@@ -1,9 +1,29 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     domains: ["localhost"],
+//   },
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "ebook-library-1.onrender.com",
+      },
+    ],
   },
 };
 
