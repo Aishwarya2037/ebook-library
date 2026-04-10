24a46e1f9fcac3f5d92380ff8c8104496889afd4
Aishwarya Jagtap <aishwarya2037@gmail.com>
2026-04-10 12:43:05 +0530

    Update server.tsx

--- elib-API/src/server.tsx
+++ elib-API/src/server.tsx
@@ -1,10 +1,25 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+// import app from "./app.js";
+// import connectDB from "./config/db.js";
+
+// connectDB();
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running on port ${process.env.PORT}`);
+// });
+
+
 import dotenv from "dotenv";
 dotenv.config();
+
 import app from "./app.js";
 import connectDB from "./config/db.js";
 
+const PORT = process.env.PORT || 3100;
+
 connectDB();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
