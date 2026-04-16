c528663219da7a1bb420e706a616601a454ff3db
Aishwarya Jagtap <aishwarya2037@gmail.com>
2026-04-16 17:55:07 +0530

    Update Footer.tsx

--- elibApp/elib-app/src/components/Footer.tsx
+++ elibApp/elib-app/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const footer = () => {
   return (
     <footer className="border-t border-gray-300 p-5 text-center">
-      <span>@ 2026 elibrary&#00 Book All reserved.</span>
+      <span>© 2026 eLibrary. All rights reserved.</span>
     </footer>
   );
 };
