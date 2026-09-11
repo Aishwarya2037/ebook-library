b5bb9d0bff3d347af5305fa85544933a95271a01
Aishwarya Jagtap <aishwarya2037@gmail.com>
2026-09-11 17:11:24 +0530

    Update Read Book button styling

--- elibApp/elib-app/src/app/book/[bookId]/components/DownloadButton.tsx
+++ elibApp/elib-app/src/app/book/[bookId]/components/DownloadButton.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 const DownloadButton = ({ bookId }: { bookId: string }) => {
-  const pdfUrl = `https://ebook-library-wbmv.onrender.com/api/books/read/${bookId}`;
+  const pdfUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/books/read/${bookId}`;
 
   return (
     <a
       href={pdfUrl}
       target="_blank"
       rel="noopener noreferrer"
-      className="inline-block px-5 py-3 bg-[#DA3D20] text-white rounded-lg font-semibold cursor-pointer"
+      className="inline-flex items-center justify-center px-6 py-3 bg-[#F8843F] text-white rounded-lg font-semibold text-base hover:opacity-90 transition duration-200"
     >
       Read Book
     </a>
